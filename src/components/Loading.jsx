8d1541d798bed6a925997f6a0414c892ff3e7378
MSR-07 <102695412+MSR-07@users.noreply.github.com>
2023-05-15 15:28:30 +0500

    Add components

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='container'>
+        <div className='row align-items-center m-5'></div>
+            <h1 className='text-center col-12 '>Loading...</h1>
+        </div>
+    );
+}
+
+export default Loading;
